174adf84010beba5524636854c65d8d9c4e4bdd4
Gerson Cruz <gcruz1776@gmail.com>
2019-03-08 18:23:50 -0600

    added data to MySQL

---
+++ MySQL/seeds.sql
@@ -0,0 +1,16 @@
+use bamazon_db;
+INSERT INTO products
+(product_name,
+department_name,
+price,
+stock_quantity)
+VALUES ('headphones','electronics', 15, 1000),
+('ps4','electronics', 400, 1500),
+('bananas','food', .49, 5000),
+('apples','food', .79, 4000),
+('shirts','clothing', 25, 900),
+('pants','clothing', 35, 750),
+('sweaters','clothing', 73, 350),
+('stapler','office supplies', 2, 1100),
+('scissors','office supplies', 3, 530),
+('pencils','office supplies', .50, 7000);
